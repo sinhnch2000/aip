86a76a4d29625084987bc6ffec8a5193d110b3e1
This PC <sinhnch2000@gmail.com>
2023-12-30 11:26:15 +0700

    bla

--- source/res/src/train.py
+++ source/res/src/train.py
@@ -2,10 +2,8 @@ import sys
 import os
 import argparse
 
-sys.path.insert(0, '/kaggle/working/aip/source/res')  # Add root directory here
-
-from src.data.dataloader_GradRes import ResDataLoader
-from src.data.dataloader_GradSearch import StateDataLoader
+from src.dataloader_GradRes import ResDataLoader
+from src.dataloader_GradSearch import StateDataLoader
 from training_loop import Trainer
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
